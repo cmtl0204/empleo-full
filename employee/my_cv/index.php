d5c7d6fe4093f4dbc3df6a90dad0406cb5e53acd
cmtl0204 <cesar.tamayo0204@gmail.com>
2024-08-02 18:41:43 -0500

    change fpdf

--- employee/my_cv/index.php
+++ employee/my_cv/index.php
@@ -1,9 +1,23 @@
 <?php
-require('./fpdf.php');
+require('html_table.php');
+require '../.././constants/db_config.php';
+require '../constants/check-login.php';
 
-$pdf=new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'HOLA YAVIRAC');
-$pdf->Output();
+if ($user_online == "true") {
+if ($myrole == "employee") {
+}else{
+header("location:../");
+}
+}else{
+header("location:../");
+}
+
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stmt = $conn->prepare("SELECT * FROM tbl_users WHERE member_no = '$myid'");
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+$pdf->Output(''.$myfname.' '.$mylname.' CV.pdf', 'I');
 ?>
