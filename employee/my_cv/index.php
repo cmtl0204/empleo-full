4fad1608d37e477bf92e04826a7c534014a5c7fb
cmtl0204 <cesar.tamayo0204@gmail.com>
2024-08-02 18:42:48 -0500

    change fpdf

--- employee/my_cv/index.php
+++ employee/my_cv/index.php
@@ -1,23 +1,5 @@
 <?php
 require('html_table.php');
-require '../.././constants/db_config.php';
-require '../constants/check-login.php';
-
-if ($user_online == "true") {
-if ($myrole == "employee") {
-}else{
-header("location:../");
-}
-}else{
-header("location:../");
-}
-
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$stmt = $conn->prepare("SELECT * FROM tbl_users WHERE member_no = '$myid'");
-$stmt->execute();
-$result = $stmt->fetchAll();
 
 $pdf->Output(''.$myfname.' '.$mylname.' CV.pdf', 'I');
 ?>
