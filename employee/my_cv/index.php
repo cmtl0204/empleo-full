1d0e198f1bb9b8bb744346f06f8608d69bacb917
cmtl0204 <cesar.tamayo0204@gmail.com>
2024-08-02 18:55:51 -0500

    change fpdf

--- employee/my_cv/index.php
+++ employee/my_cv/index.php
@@ -12,7 +12,7 @@ header("location:../");
 header("location:../");
 }
 
-$conn = new PDO("mysql:host=207.244.242.219;dbname=job_board", 'root', 'dxA9aj.4iZBmWy,)');
+$conn = new PDO("mysql:host=207.244.242.219;dbname=job_board", 'root2', 'dxA9aj.4iZBmWy,)');
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $pdf=new PDF_HTML_Table();
