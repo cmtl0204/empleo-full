580e5b733faa33d1f8a47bc2cc5feb8b1d8f05ac
cmtl0204 <cesar.tamayo0204@gmail.com>
2024-08-02 19:00:48 -0500

    change fpdf

--- employee/my_cv/index.php
+++ employee/my_cv/index.php
@@ -4,19 +4,24 @@ require '../../constants/db_config.php';
 require '../constants/check-login.php';
 
 if ($user_online == "true") {
-if ($myrole == "employee") {
-}else{
-header("location:../");
-}
-}else{
-header("location:../");
+    if ($myrole == "employee") {
+    } else {
+        header("location:../");
+    }
+} else {
+    header("location:../");
 }
 
-//$conn = new PDO("mysql:host=207.244.242.219;dbname=job_board", 'root', 'dxA9aj.4iZBmWy,)');
+try {
+    $conn = new PDO("mysql:host=207.244.242.219;dbname=job_board", 'root', 'dxA9aj.4iZBmWy,)');
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
 
-$pdf=new PDF_HTML_Table();
+
+$pdf = new PDF_HTML_Table();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'HOLA YAVIRAC');
+$pdf->SetFont('Arial', 'B', 16);
+$pdf->Cell(40, 10, 'HOLA YAVIRAC');
 $pdf->Output();
 ?>
